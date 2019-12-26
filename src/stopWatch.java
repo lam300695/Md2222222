77d5af89adbb245658991488108a97dd736bed7f
lam300695 <lam300695@gmail.com>
2019-12-26 08:54:42 +0700

    Md2

---
+++ src/stopWatch.java
@@ -0,0 +1,43 @@
+import java.util.Date;
+
+public class stopWatch {
+    private long startTime = 0;
+    private long endTime = 0;
+
+    public stopWatch() {
+    }
+
+    public stopWatch(long startTime, long endTime) {
+        this.startTime = startTime;
+        this.endTime = endTime;
+    }
+
+    public long getStartTime() {
+        return startTime;
+    }
+
+    public void setStartTime(long startTime) {
+        this.startTime = startTime;
+    }
+
+    public long getEndTime() {
+        return endTime;
+    }
+
+    public void setEndTime(long endTime) {
+        this.endTime = endTime;
+    }
+
+    public long start() {
+        return this.startTime = System.currentTimeMillis();
+    }
+
+    public long stop() {
+        return this.endTime = System.currentTimeMillis();
+    }
+
+    public long getElapsedTime() {
+        long result = endTime - startTime;
+        return result;
+    }
+}
