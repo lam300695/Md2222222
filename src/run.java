f1463f62b4f2052b0b50894af73da1011bb17090
lam300695 <lam300695@gmail.com>
2019-12-26 09:43:21 +0700

    ...

--- src/run.java
+++ src/run.java
@@ -1,5 +1,21 @@
 public class run {
     public static void main(String[] args) {
+        stopWatch stop = new stopWatch(0, 0);
 
+        System.out.println("Start " + stop.start());
+        System.out.println("-----");
+        System.out.println("-----");
+        System.out.println("-----");
+        System.out.println("-----");
+        System.out.println("-----");
+        System.out.println("-----");
+        System.out.println("-----");
+        System.out.println("-----");
+        System.out.println("-----");
+        System.out.println("-----");
+        System.out.println("-----");
+        System.out.println("Stop " + stop.stop());
+
+        System.out.println("stop - start : " + stop.getElapsedTime());
     }
 }
